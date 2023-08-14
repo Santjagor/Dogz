947894e1bce900ea45704d2fe9c04c28a8f9a208
Santiago Martin Solavaggione <santirigore@gmail.com>
2023-08-13 23:47:47 -0300

    13-08-2023

--- client/src/components/Landing/Landing.jsx
+++ client/src/components/Landing/Landing.jsx
@@ -1,10 +1,13 @@
 import styles from './Landing.module.css'
+import Overlay from '../Overlay/Overlay'
 
 export default function Landing({ access }) {
     return (
-        <div>
-            <p>Landing</p>
-            <button onClick={access}>Enter</button>
+        <div className={styles.container}>
+            <Overlay></Overlay>
+            <img className={styles.dogz} src="https://i.postimg.cc/pXqPwdv0/DogZ.png" alt="" />
+            <img className={styles.welcome} src="https://i.postimg.cc/TPNTsTz8/Welcome.png" alt="" />
+            <button className={styles.enter} onClick={access}>Enter</button>
         </div>
     )
 }
--- client/src/components/Overlay/Overlay.jsx
+++ client/src/components/Overlay/Overlay.jsx
@@ -1,11 +1,13 @@
 import styles from './Overlay.module.css'
+import { useLocation } from 'react-router-dom'
 export default function Overlay() {
+    const location = useLocation()
     return (
         <div>
             <div className={styles.left_bar}></div>
             <div className={styles.right_bar}></div>
-            <img className={styles.left_bot_dog} src='https://i.postimg.cc/pX22cFwh/dog0.png' alt="" />
-            <img className={styles.right_bot_dog} src='https://i.postimg.cc/Dw07nKy7/dog1.png' alt="" />
+            {location.pathname !== '/' && <img className={styles.left_bot_dog} src='https://i.postimg.cc/pX22cFwh/dog0.png' alt="" />}
+            {location.pathname !== '/' && <img className={styles.right_bot_dog} src='https://i.postimg.cc/Dw07nKy7/dog1.png' alt="" />}
         </div>
     )
 }
